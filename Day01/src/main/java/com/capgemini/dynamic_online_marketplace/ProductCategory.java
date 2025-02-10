52c53d41f40ee4441d2afd57bc313e6bfd25c860
omtiwari52 <tiwariom900@gmail.com>
2025-02-10 22:48:59 +0530

    [Om Tiwari] refactor: code refactor📦

--- Day01/src/main/java/com/capgemini/dynamic_online_marketplace/ProductCategory.java
+++ Day01/src/main/java/com/capgemini/dynamic_online_marketplace/ProductCategory.java
@@ -5,10 +5,12 @@ abstract class ProductCategory {
     //define attribute
     private String categoryName;
 
+    //constructor
     ProductCategory(String categoryName){
         this.categoryName = categoryName;
     }
 
+    //method to get category name
     public String getCategoryName() {
         return categoryName;
     }
