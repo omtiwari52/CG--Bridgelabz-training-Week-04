011f7097d73b83b0936f9b209312f27a37ecd987
omtiwari52 <tiwariom900@gmail.com>
2025-02-12 21:19:00 +0530

    [Om Tiwari] refactor: read input source file and write destination file test code refactored📦

--- Day03/src/main/java/com/capgemini/file_handling_read_and_write_a_text_file/ReadInputFileAndWriteDestination.java
+++ Day03/src/main/java/com/capgemini/file_handling_read_and_write_a_text_file/ReadInputFileAndWriteDestination.java
@@ -7,7 +7,7 @@ public class ReadInputFileAndWriteDestination {
     //method to read file
     public static void readFile(String sourceFileName, String destinationFileName){
         //storing in file path
-        String myFile = "src/main/java/file_handling_read_and_write_a_text_file/" + sourceFileName;
+        String myFile = "src/main/java/com/capgemini/file_handling_read_and_write_a_text_file/" + sourceFileName;
 
         try (BufferedReader br = new BufferedReader(new FileReader(myFile))){
             String line; //line contains each line
@@ -24,9 +24,18 @@ public class ReadInputFileAndWriteDestination {
     //method to write in file
     public static void writeFile(String fileName, String content){
         String myFile = "src/main/java/file_handling_read_and_write_a_text_file/" + fileName;
-        try (FileWriter newFile = new FileWriter(myFile,true)){
-            newFile.write(content + "\n");
-        } catch (IOException e) {
+        File file = new File(myFile);
+        try {
+            // Ensure the file exists
+            if (!file.exists()) {
+                file.getParentFile().mkdirs(); // Create directories if they do not exist
+                file.createNewFile(); // Create the file
+            }
+            try (FileWriter newFile = new FileWriter(myFile,true)){
+                newFile.write(content + "\n");
+            }
+        }
+        catch (IOException e) {
             System.out.println("Exception caught "+e.getMessage());
         }
     }
