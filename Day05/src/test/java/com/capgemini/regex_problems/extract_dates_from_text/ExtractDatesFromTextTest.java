87a1de060079b8974ff50058dea6220300e41bf0
omtiwari52 <tiwariom900@gmail.com>
2025-02-15 20:15:46 +0530

    [Om Tiwari] add: extract dates from text test code file added✨

---
+++ Day05/src/test/java/com/capgemini/regex_problems/extract_dates_from_text/ExtractDatesFromTextTest.java
@@ -0,0 +1,20 @@
+package com.capgemini.regex.extract_dates_from_text;
+
+import com.capgemini.regex.extract_emails_from_text.ExtractEmailAddress;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExtractDatesFromTextTest {
+    @Test
+    void checkEmailAddressAtParticularIndex(){
+        String text = "12/04/2004 this is the date when i born just kidding the real date us 24/04/2003";
+        assertEquals("12/04/2004", ExtractDatesFromText.extractDates(text).get(0));
+    }
+
+    @Test
+    void checkEmailAddressAtParticularIndex2(){
+        String text = "12/04/2004 this is the date when i born just kidding the real date us 24/04/2003";
+        assertEquals("24/04/2003", ExtractDatesFromText.extractDates(text).get(1));
+    }
+}
